c35ca08b72c298f2a745cf2008bdbc93828e39b0
NitinSemwal <140317834+NitinSemwal2605@users.noreply.github.com>
2024-08-12 21:08:50 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'; // Importing Routes instead of Switch
 import { FlashcardProvider } from './components/FlashcardContext';
-import FlipCardsSection from './components/Flashcard.jsx';
-import Navbar from './components/MyNavBar.jsx';
+import FlipCardsSection from './components/Flashcard'; // Ensure the file name matches
+import Navbar from './components/MyNavBar'; // Ensure the file name matches
 import ProtectedRoute from './components/ProtectedRoute';
 import AdminDashboard from './components/Dashboard';
 
@@ -11,19 +11,18 @@ function App() {
     <Router>
       <div>
         <FlashcardProvider>
-        
-        <Navbar />
-        <Routes> {/* Use Routes instead of Switch */}
-          <Route path="/" element={<FlipCardsSection />} /> {/* Use element instead of component */}
-          <Route
-          path="/dashboard"
-          element={
-            <ProtectedRoute>
-              <AdminDashboard />
-            </ProtectedRoute>
-          }
-        />
-        </Routes>
+          <Navbar />
+          <Routes> {/* Use Routes instead of Switch */}
+            <Route path="/" element={<FlipCardsSection />} /> {/* Use element instead of component */}
+            <Route
+              path="/dashboard"
+              element={
+                <ProtectedRoute>
+                  <AdminDashboard />
+                </ProtectedRoute>
+              }
+            />
+          </Routes>
         </FlashcardProvider>
       </div>
     </Router>
